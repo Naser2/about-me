f074691b7434ad67d0be677c7402ef1e94a0cf90
Nasser Sanou <wardjamnaty@yahoo.fr>
2017-01-15 21:43:11 -0500

    re-config server

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+
 var express = require('express');
-var app = express();
+var app     = express();
 
-app.use(express.static(__dirname + '/'));
+app.set('port', (process.env.PORT || 8080));
 
-app.listen(process.env.PORT || 8080), function() {
-  console.log('listening on *:8080');
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
+
+
+// Test
+// var express = require('express');
+// var app = express();
+//
+// app.use(express.static(__dirname + '/'));
+//
+// app.listen(process.env.PORT || 8080), function() {
+//   console.log('listening on *:8080');
+// });
