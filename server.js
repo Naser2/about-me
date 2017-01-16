eba8a81a0ba86acffb1892ccad46eec0bd7e656a
Nasser Sanou <wardjamnaty@yahoo.fr>
2017-01-15 21:32:30 -0500

    re-config the production/Add .en

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var server = express();
+var app = express();
 
-server.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/'));
 
-server.listen(process.env.PORT || 8080), function() {
+app.listen(process.env.PORT || 8080), function() {
   console.log('listening on *:8080');
 });
