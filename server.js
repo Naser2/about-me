f59b93ce4872e5d0470a4743dc38bcb1b85c6da3
Nasser Sanou <wardjamnaty@yahoo.fr>
2017-01-17 17:39:06 -0500

    fix node version

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.get('/', (req, res) => {
 
 // app.use(express.static(__dirname + '/'));
 
-const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`Runing on port ${port}`);
+app.listen(process.env.PORT || 8080, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
