e4f556ace88393927665a138e6b960230a0f2431
Nasser Sanou <wardjamnaty@yahoo.fr>
2017-01-17 17:13:30 -0500

    config

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-// //Libraries
-// import React from 'react';
-// import { render } from 'react-dom';
-//
-// // CSS
-// import './css/style.css';
-//
-// // Routes
-// import routes from './router';
-//
-// // Render
-// render(
-//   routes,
-//   document.getElementById('root')
-// );
+//Libraries
+import React from 'react';
+import { render } from 'react-dom';
+
+// CSS
+import './css/style.css';
+
+// Routes
+import routes from './router';
+
+// Render
+render(
+  routes,
+  document.getElementById('root')
+);
